0d4a58eca113148c9abe6af001df2004f74441c7
greenbender <byron.platt@gmail.com>
2019-04-08 16:46:57 -0700

    Fix missing README in setup.py
    Change to restructured text README

--- tools/njic/setup.py
+++ tools/njic/setup.py
@@ -3,7 +3,7 @@ import glob
 from setuptools import setup, find_packages
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 
