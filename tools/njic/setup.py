c82d29f03196f4cd6196eecf86089e799ffa720d
greenbender <byron.platt@gmail.com>
2019-04-08 16:48:42 -0700

    Version bump

--- tools/njic/setup.py
+++ tools/njic/setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='nji',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     package_data={'nji': ['templates/*']},
